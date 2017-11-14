b95791c8b7794dfac12c4e9c88e25f1ca109b737
longchass <lehongkhanhphuong@yahoo.com.vn>
2017-11-14 15:39:51 +1100

    update

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # project neightborhood
+##project neighborhood map udacity
+this project neightborhood map highlight few of the most beautiful places in Australia
+this -project use Jquery.js : [https://api.jquery.com/]
+     -knockout.js: [http://knockoutjs.com/]
+     -foursquare API: [https://developer.foursquare.com/]
+     -most important google map api: [https://developers.google.com/maps/]
+how to use this:
+unzip file
+run index.html
+click on the markers/names
+see information
+website : pending
+#OR
+try it live here:[https://longchass.github.io/project-neightborhood/]
+
+
+
+
+
+
+update: v1 initialized index.html, frame.js, knockout.js
+update: v2 applied google map API keys adding map markers
+update: v3 applied show lisiting, hide listing, crate side nav, added location info
+update: v4 removed show listing, hide lisitng for optimization purpose
+update: v5 implement knockout.js, foursquare api, created search bar
+update: v6 created README.txt
--- js/frame.js
+++ js/frame.js
@@ -1,184 +1,183 @@
-          var uluru = {
-              lat: -25.363,
-              lng: 131.044
-          };
-          var markers = [];
+var uluru = {
+    lat: -25.363,
+    lng: 131.044
+};
+var markers = [];
 
-var initialLocations = [
+var initialLocations = [{
+        lat: -16.8804789,
+        lng: 145.5768562,
+        info: "Cairns, Queensland",
+        id: "1"
+    },
     {
-                  lat: -16.8804789,
-                  lng: 145.5768562,
-                  info: "Cairns, Queensland",
-                  id : "1"
-              },
-              {
-                  lat: -20.1860205,
-                  lng: 148.4466541,
-                  info: "The Whitsundays, Queensland",
-                                    id : "1"
+        lat: -20.1860205,
+        lng: 148.4466541,
+        info: "The Whitsundays, Queensland",
+        id: "1"
 
-              },
-              {
-                  lat: -25.2461523,
-                  lng: 152.5909349,
-                  info: "Fraser Island, Queensland",
-                                    id : "2"
+    },
+    {
+        lat: -25.2461523,
+        lng: 152.5909349,
+        info: "Fraser Island, Queensland",
+        id: "2"
 
-              },
-              {
-                  lat: -26.4001193,
-                  lng: 153.078467,
-                  info: "Noosa Heads, Queensland",
-                                    id : "3"
+    },
+    {
+        lat: -26.4001193,
+        lng: 153.078467,
+        info: "Noosa Heads, Queensland",
+        id: "3"
 
-              },
-              {
-                  lat: -27.5735727,
-                  lng: 151.8568419,
-                  info: "Toowoomba, Queensland",
-                                    id : "4"
+    },
+    {
+        lat: -27.5735727,
+        lng: 151.8568419,
+        info: "Toowoomba, Queensland",
+        id: "4"
 
-              },
-              {
-                  lat: -29.4337257,
-                  lng: 153.3245816,
-                  info: " Yamba, New South Wales",
-                                    id : "5"
+    },
+    {
+        lat: -29.4337257,
+        lng: 153.3245816,
+        info: " Yamba, New South Wales",
+        id: "5"
 
-              },
-              {
-                  lat: -31.5543097,
-                  lng: 159.0361234,
-                  info: "Lord Howe Island, New South Wales",
-                                    id : "6"
+    },
+    {
+        lat: -31.5543097,
+        lng: 159.0361234,
+        info: "Lord Howe Island, New South Wales",
+        id: "6"
 
-              },
-              {
-                  lat: -33.7097844,
-                  lng: 150.3038632,
-                  info: "Leura, New South Wales",
-                                    id : "7"
+    },
+    {
+        lat: -33.7097844,
+        lng: 150.3038632,
+        info: "Leura, New South Wales",
+        id: "7"
 
-              },
-              {
-                  lat: -33.847927,
-                  lng: 150.6517805,
-                  info: "Sydney, New South Wales",
-                                    id : "8"
+    },
+    {
+        lat: -33.847927,
+        lng: 150.6517805,
+        info: "Sydney, New South Wales",
+        id: "8"
 
-              },
-              {
-                  lat: -34.6711099,
-                  lng: 150.8116032,
-                  info: "Kiama, New South Wales",
-                                    id : "9"
+    },
+    {
+        lat: -34.6711099,
+        lng: 150.8116032,
+        info: "Kiama, New South Wales",
+        id: "9"
 
-              },
-              {
-                  lat: -35.0360936,
-                  lng: 150.6321875,
-                  info: "Huskisson, New South Wales",
-                                    id : "10"
+    },
+    {
+        lat: -35.0360936,
+        lng: 150.6321875,
+        info: "Huskisson, New South Wales",
+        id: "10"
 
-              },
-              {
-                  lat: -36.702585,
-                  lng: 146.961849,
-                  info: "Bright, Victoria",
-                                    id : "11"
+    },
+    {
+        lat: -36.702585,
+        lng: 146.961849,
+        info: "Bright, Victoria",
+        id: "11"
 
-              },
-              {
-                  lat: -38.3309285,
-                  lng: 144.676316,
-                  info: "Mornington Peninsula, Victoria",
-                                    id : "12"
+    },
+    {
+        lat: -38.3309285,
+        lng: 144.676316,
+        info: "Mornington Peninsula, Victoria",
+        id: "12"
 
-              },
-              {
-                  lat: -36.7561786,
-                  lng: 144.2626423,
-                  info: "Bendigo, Victoria",
-                                    id : "13"
+    },
+    {
+        lat: -36.7561786,
+        lng: 144.2626423,
+        info: "Bendigo, Victoria",
+        id: "13"
 
-              },
-              {
-                  lat: -38.1481387,
-                  lng: 144.3453713,
-                  info: "Geelong, Victoria",
-                                    id : "14"
+    },
+    {
+        lat: -38.1481387,
+        lng: 144.3453713,
+        info: "Geelong, Victoria",
+        id: "14"
 
-              },
-              {
-                  lat: -37.826317,
-                  lng: 140.7456247,
-                  info: "Mount Gambier, South Australia",
-                                    id : "15"
+    },
+    {
+        lat: -37.826317,
+        lng: 140.7456247,
+        info: "Mount Gambier, South Australia",
+        id: "15"
 
-              },
-              {
-                  lat: -35.9313167,
-                  lng: 139.4527674,
-                  info: "Coorong, South Australia",
-                                    id : "16"
+    },
+    {
+        lat: -35.9313167,
+        lng: 139.4527674,
+        info: "Coorong, South Australia",
+        id: "16"
 
-              },
-              {
-                  lat: -36.1044201,
-                  lng: 138.8220108,
-                  info: "Launceston, Tasmania",
-                                    id : "17"
+    },
+    {
+        lat: -36.1044201,
+        lng: 138.8220108,
+        info: "Launceston, Tasmania",
+        id: "17"
 
-              },
-              {
-                  lat: -42.4774711,
-                  lng: 147.2660684,
-                  info: "Wineglass Bay, Tasmania",
-                                    id : "18"
+    },
+    {
+        lat: -42.4774711,
+        lng: 147.2660684,
+        info: "Wineglass Bay, Tasmania",
+        id: "18"
 
-              },
-              {
-                  lat: -33.8612197,
-                  lng: 121.8829606,
-                  info: "Esperance, Western Australia",
-                                    id : "19"
+    },
+    {
+        lat: -33.8612197,
+        lng: 121.8829606,
+        info: "Esperance, Western Australia",
+        id: "19"
 
-              },
-              {
-                  lat: -35.0280667,
-                  lng: 117.8754271,
-                  info: "Albany, Western Australia",
-                                    id : "20"
+    },
+    {
+        lat: -35.0280667,
+        lng: 117.8754271,
+        info: "Albany, Western Australia",
+        id: "20"
 
-              },
-              {
-                  lat: -32.0069946,
-                  lng: 115.4689695,
-                  info: "Broome, Western Australia",
-                                    id : "21"
+    },
+    {
+        lat: -32.0069946,
+        lng: 115.4689695,
+        info: "Broome, Western Australia",
+        id: "21"
 
-              },
-              {
-                  lat: -12.4258916,
-                  lng: 130.8632674,
-                  info: "Darwin, Northern Territory",
-                                    id : "22"
+    },
+    {
+        lat: -12.4258916,
+        lng: 130.8632674,
+        info: "Darwin, Northern Territory",
+        id: "22"
 
-              },
-              {
-                  lat: -14.2016824,
-                  lng: 132.3189422,
-                  info: "Wineglass Bay, Tasmania",
-                                    id : "23"
+    },
+    {
+        lat: -14.2016824,
+        lng: 132.3189422,
+        info: "Wineglass Bay, Tasmania",
+        id: "23"
 
-              },
-              {
-                lat: -14.2013496,
-                lng: 132.3189414,
-                info: "Katherine Gorge, Northern Territory",
-                                  id : "24"
+    },
+    {
+        lat: -14.2013496,
+        lng: 132.3189414,
+        info: "Katherine Gorge, Northern Territory",
+        id: "24"
 
-              }
+    }
 
 
 ];
@@ -193,8 +192,31 @@ var Location = function(data) {
     self.id = data.id;
     self.street = "";
     self.city = "";
+    self.incase = "";
     self.visible = ko.observable(true);
-    self.contentString = '<div class="info-window-content"><div class="title"><b>' + data.info + "</b></div>";
+    var foursquareURL = "https://api.foursquare.com/v2/venues/search?ll=" + this.lat + "," + this.lng + "&client_id=" + clientID + "&client_secret=" + clientSecret + "&v=20160118" + "&query=" + this.info;
+    $.getJSON(foursquareURL).done(function(data) {
+        var results = data.response.venues[0];
+        if (typeof results !== "undefined"){
+        self.street = results.location.formattedAddress[0];
+        if (typeof self.street === "undefined") {
+            self.street = "data unavailable";
+        }
+        self.city = results.location.formattedAddress[1];
+        if (typeof self.city === "undefined") {
+            self.city = "data unavailable";
+        }
+        if (Boolean(self.street) == false && Boolean(self.city) == false && typeof results === "undefined") {
+            self.incase = "data unavailable"
+        }
+      }
+    }).fail(function() {
+        alert("foursquare can't load");
+    });
+    self.contentString = "<div class=\"info-window-content\"><div class=\"title\"><b>" + self.info + "</b></div>" +
+        "<div class=\"content\">" + self.street + "</div>" +
+        "<div class=\"content\">" + self.city + "</div>" +
+        "<div class=\"content\">" + self.incase + "</div>";
     self.infoWindow = new google.maps.InfoWindow({
         content: self.contentString
     });
@@ -202,8 +224,8 @@ var Location = function(data) {
     self.marker = new google.maps.Marker({
         position: new google.maps.LatLng(data.lat, data.lng),
         map: map,
-        title: data.info,
-        id : data.id
+        title: self.info,
+        id: data.id
 
     });
     self.showMarker = ko.computed(function() {
@@ -215,6 +237,15 @@ var Location = function(data) {
         return true;
     }, self);
     self.marker.addListener('click', function() {
+        if (Boolean(self.street) == false && Boolean(self.city) == false && typeof results === "undefined") {
+            self.incase = "data unavailable"
+        }
+        self.contentString = "<div class=\"info-window-content\"><div class=\"title\"><b>" + self.info + "</b></div>" +
+            "<div class=\"content\">" + self.street + "</div>" +
+            "<div class=\"content\">" + self.city + "</div>" +
+
+            "<div class=\"content\">" + self.incase + "</div>";
+
 
         self.infoWindow.setContent(self.contentString);
 
@@ -231,22 +262,22 @@ var Location = function(data) {
 };
 
 function AppViewModel() {
-  var styles = [{
-              featureType: "water",
-              stylers: [{
-                  color: "#19A0D8"
-              }]
-          }, {
-              featureType: "adminstrative",
-              elementType: "labels.text.stroke",
-              stylers: [{
-                      color: "#000000"
-                  },
-                  {
-                      weight: 6
-                  }
-              ]
-          }];
+    var styles = [{
+        featureType: "water",
+        stylers: [{
+            color: "#19A0D8"
+        }]
+    }, {
+        featureType: "adminstrative",
+        elementType: "labels.text.stroke",
+        stylers: [{
+                color: "#000000"
+            },
+            {
+                weight: 6
+            }
+        ]
+    }];
     var self = this;
 
     self.searchTerm = ko.observable("");
@@ -255,13 +286,23 @@ function AppViewModel() {
 
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 4,
-         styles: styles,
-              center: uluru,
-              mapTypeControl: false
+        styles: styles,
+        center: uluru,
+        mapTypeControl: false
     });
+    clientID = "ZN2UPAIEOK2JTS3PNGF42FLBZTLWDXLRK0DLIKGW2MIRIIJ1";
+    clientSecret = "QAXIUYYAZKC3JUMBMJ1XFV320SU4IL5EK3GOY502QNYNMLW5";
+
+
+
     initialLocations.forEach(function(locationItem) {
         self.locationList.push(new Location(locationItem));
     });
+    self.contentString = '<div class="info-window-content"><div class="title"><b>' + self.info + "</b></div>" +
+        '<div class="content">' + self.street + "</div>" +
+        '<div class="content">' + self.city + "</div>"
+
+    ;
     self.filteredList = ko.computed(function() {
         var filter = self.searchTerm().toLowerCase();
         if (!filter) {
@@ -284,26 +325,22 @@ function startApp() {
     ko.applyBindings(new AppViewModel());
 }
 
-function errorHandling() {
-    alert("Google Maps has failed to load. Please check your internet connection and try again.");
+
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "25%";
+    document.getElementById("map").style.width = "75%";
+    document.getElementById("map").style.marginLeft = "25%";
 }
 
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0%";
+    document.getElementById("map").style.width = "100%";
+    document.getElementById("map").style.marginLeft = "0%";
 
+}
 
-
-
-      function openNav() {
-          document.getElementById("mySidenav").style.width = "25%";
-          document.getElementById("map").style.width = "75%";
-          document.getElementById("map").style.marginLeft = "25%";
-      }
-
-      function closeNav() {
-          document.getElementById("mySidenav").style.width = "0%";
-          document.getElementById("map").style.width = "100%";
-          document.getElementById("map").style.marginLeft = "0%";
-
-      }
-function error(){
-  alert("google map can't load")
+function error() {
+    alert("google map can't load")
 }
